ea037bc6148c61958d22551e1438ee7abec520ce
Your Name <you@example.com>
2023-05-04 10:25:02 +0300

    feat: make 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = fs.promises;
 
+fsPromises.rm('files-copy', {recursive: true, force: true})
+  .then(() => {
+    fsPromises.mkdir(__dirname + path.sep + 'files-copy', {
+      recursive: true
+    })
+  }).then(() => {
+    fsPromises.readdir(__dirname + path.sep + 'files')
+      .then((filenames) => {
+        for (let filename of filenames) {
+          fsPromises.copyFile(__dirname + path.sep + 'files' + path.sep + filename, __dirname + path.sep + 'files-copy' + path.sep + filename)
+        }
+      })
+  }).catch(() => {
+    console.log('failed to create directory');
+  });
